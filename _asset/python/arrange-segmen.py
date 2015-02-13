a63001dc9286ecc119bfa9eff45f145320b3af19
Samas Lin <samas0120@gmail.com>
2015-02-13 22:25:10 +0800

    purify fan list

--- _asset/python/arrange-segmen.py
+++ _asset/python/arrange-segmen.py
@@ -6,17 +6,14 @@ sys.setdefaultencoding("utf-8")
 
 def arrange_segment(file_prefix, segment_number):
 
-	sys.stdout.write('processing segment ' + segment_number + '......')
+	sys.stdout.write('processing segment ' + str(segment_number) + '......')
 	source_file = '../data/' + file_prefix + '_' + '57613404340' + '_' + str('%05d' %segment_number) + '.csv'
 	target_file = '../data/' + file_prefix + '_' + '57613404340' + '_' + str('%05d' %(segment_number + 1))+ '.csv'
 	temp_content = ''
 
-	if segment_number == 1:
-		input_file = open(source_file, 'r')
-		file_content = input_file.read()
-		input_file.close()
-	else:
-		file_content = temp_content
+	input_file = open(source_file, 'r')
+	file_content = input_file.read()
+	input_file.close()
 
 	next_file = open(target_file, 'r')
 	for line in next_file.readlines():
@@ -30,5 +27,5 @@ def arrange_segment(file_prefix, segment_number):
 	print('done')
 
 if __name__ == '__main__':
-	for segment_number in range(1,488):
+	for segment_number in range(1,487):
 		arrange_segment('fan_list', segment_number)
