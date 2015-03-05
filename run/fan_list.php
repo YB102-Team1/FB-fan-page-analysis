447f3b7d15e97763aec4dbe2dc21dc0d64837844
Samas Lin <samas0120@gmail.com>
2015-03-05 15:55:10 +0800

    特殊字串處理

--- run/fan_list.php
+++ run/fan_list.php
@@ -3,8 +3,9 @@ $file_prefix = 'fan_list_57613404340_';
 $link = new mysqli('localhost', 'team1', 'yb102', 'YB102_Team1');
 $link->query("SET NAMES UTF8");
 for ($segment = 1; $segment <= 487; $segment++) {
-    $file_path = $_SERVER['DOCUMENT_ROOT'].'/_asset/data/fan_list/'.$file_prefix.sprintf('%05d', $segment).'.csv';
+    $file_path = $_SERVER['DOCUMENT_ROOT'].'/_asset/data/fan_list/purified/'.$file_prefix.sprintf('%05d', $segment).'.csv';
     $file = @fopen($file_path, "r");
+    $is_first = true;
     while (!feof($file)) {
         $data_string = fgets($file);
         str_replace("\n", "", $data_string);
@@ -14,11 +15,15 @@ for ($segment = 1; $segment <= 487; $segment++) {
         $column3 = $data_array[2];
         $now = date('Y-m-d H:i:s', time()+25200);
         if ($column1) {
-            $insert_sql = "INSERT INTO `fb_user` (`segment`, `fb_user_id`, `name`, `path`, `create_time`, `modify_time`) VALUES ('$segment', '$column1', '$column2', '$column3', '$now', '$now')";
-            echo "INSERT INTO `fb_user` (`segment`, `fb_user_id`, `name`, `path`, `create_time`, `modify_time`) VALUES ('$segment', '$column1', '$column2', '$column3', '$now', '$now')".'<br>';
-            $link->query($insert_sql);
+            if ($is_first) {
+                $insert_sql = "INSERT INTO `fb_user` (`segment`, `fb_user_id`, `name`, `path`, `create_time`, `modify_time`) VALUES (\"$segment\", \"$column1\", \"$column2\", \"$column3\", \"$now\", \"$now\")";
+                $is_first = false;
+            } else {
+                $insert_sql .= ", (\"$segment\", \"$column1\", \"$column2\", \"$column3\", \"$now\", \"$now\")";
+            }
         }
     }
+    $link->query($insert_sql);
     fclose($file);
 }
 ?>
